9441bfd00d01b858d3a8c756f0385fee3e829f42
André Heller <anheller6@gmail.com>
2015-10-24 12:36:38 +0200

    AlertServiceTest

--- assets/tests/AlertServiceTest.ts
+++ assets/tests/AlertServiceTest.ts
@@ -1,8 +1,16 @@
 ///<reference path="../src/reference.ts" />
-describe('AlertService: ', () => {
+describe('Service: AlertService', () => {
 	
 	var alertService: application.services.AlertService,
 		mockRootScope: any;
+		
+	//Alert Types
+	var types: Array<string> = ['success','info','warning','danger'];
+	//Icons
+	var icons: Array<string> = ['ok-sign','info-sign','exclamation-sign','remove-sign'];
+	//Message
+	var msg: string = "Message!";
+	
 	
 	beforeEach(function() {
 		angular.mock.module('GAManager')
@@ -11,51 +19,49 @@ describe('AlertService: ', () => {
 	beforeEach(angular.mock.inject(function($rootScope, _alertService_: application.services.AlertService) {
 		mockRootScope = $rootScope;
 		alertService = _alertService_;
-	}));
+	}));	
 	
-	//Alert Types
-	var types: Array<string> = ['success','info','warning','danger'];
-	//Icons
-	var icons: Array<string> = ['ok-sign','info-sign','exclamation-sign','remove-sign'];
-	
-	
-	it('Method addAlert(): Should add alert into rootScope.alerts array.', function() {
-		var msg = "test",
-			type = types[3]; //"danger"
+	describe('Method: addAlert()', () => {
 		
-		alertService.addAlert(type, msg);
+		it('Should add alert into rootScope.alerts array.', () =>{
 		
-		expect(mockRootScope.alerts).toContain(jasmine.objectContaining({
-			'msg': msg,
-			'type': type
-		}));
+			alertService.addAlert(types[3], msg);
+			
+			expect(mockRootScope.alerts).toContain(jasmine.objectContaining({
+				'msg': msg,
+				'type': types[3]
+			}));
+		});
+		
+		it('Should set specific icon based on provided type.', () => {
+			for (var i = 0; i < types.length; i++){
+				var alert = alertService.addAlert(types[i], msg); 
+				
+				expect(alert.icon).toEqual(icons[i])	
+			}
+		});
 	});
 	
 	
-	it('Method closeAlert(): Should remove alert from rootScope.alerts array.', function() {
-		var msg = "test",
-			type = types[3]; //"danger"
-		
-		var alert = alertService.addAlert(type,msg);
-		
-		expect(mockRootScope.alerts).toContain(jasmine.objectContaining({
-			'msg': msg,
-			'type': type
-		}));
-		
-		alertService.closeAlert(alert);
-		
-		expect(mockRootScope.alerts).not.toContain(jasmine.objectContaining({
-			'msg': msg,
-			'type': type
-		}));
-	});
 	
-	
-	it('Method getAlertIcon(): Should return specific icon based on provide type.', function() {
-		for (var i = 0; i < types.length; i++){
-			expect(alertService.getAlertIcon(types[i])).toEqual(icons[i])	
-		}
-	});
-	
+	describe('Method: closeAlert()', () => {
+		it('Should remove alert from rootScope.alerts array.', function() {
+			var msg = "test",
+				type = types[3]; //"danger"
+			
+			var alert = alertService.addAlert(type,msg);
+			
+			expect(mockRootScope.alerts).toContain(jasmine.objectContaining({
+				'msg': msg,
+				'type': type
+			}));
+			
+			alertService.closeAlert(alert);
+			
+			expect(mockRootScope.alerts).not.toContain(jasmine.objectContaining({
+				'msg': msg,
+				'type': type
+			}));
+		});
+	})
 });
